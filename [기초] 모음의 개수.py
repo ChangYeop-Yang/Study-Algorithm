b10eda7e6a11972a7c12bcbe30d5e098578e94ea
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2016-12-11 02:15:50 +0900

    https://www.acmicpc.net/problem/10987
    https://www.acmicpc.net/problem/10987

---
+++ [기초] 모음의 개수.py
@@ -0,0 +1,16 @@
+string = input()
+
+total = 0
+for ii in string :
+    if 'a' == ii:
+        total += 1
+    elif 'e' == ii:
+        total += 1
+    elif 'i' == ii:
+        total += 1
+    elif 'o' == ii:
+        total += 1
+    elif 'u' == ii:
+        total += 1
+
+print(total)
