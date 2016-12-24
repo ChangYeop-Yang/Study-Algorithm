7e05f4e57ea55af819498f126d0fa36e7a1fd79a
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2016-12-24 21:22:14 +0900

    https://www.acmicpc.net/problem/9316
    https://www.acmicpc.net/problem/9316

---
+++ [출력] Hello Judge.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int nn = 0;
+	cin >> nn;
+
+	for (int ii = 0; ii < nn; ii++)
+	{
+		cout << "Hello World, Judge " << ii + 1 << "!" << endl;
+	}
+
+	return 0;
+}
