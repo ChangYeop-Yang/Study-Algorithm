86a049dda57afc74d5f694600ef656f92d9e97a2
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2019-01-11 00:18:08 +0900

    https://www.acmicpc.net/problem/15665
    https://www.acmicpc.net/problem/15665

---
+++ [재귀] N과 M (11).cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+vector<int> number;
+set<vector<int> > answer;
+
+void reculsive(const int length, const int goal, int index, vector<int> bucket) {
+
+	if (index > goal)	{ return; }
+	if (index == goal)	{ answer.insert(bucket); return; }
+
+	for (int ii = 0; ii < length; ii++) {
+		bucket[index] = number[ii];
+		reculsive(length, goal, index + 1, bucket);
+	}
+}
+
+int main(void)
+{
+	pair<int, int> input = make_pair(0, 0);
+	cin >> input.first >> input.second;
+
+	number = vector<int>(input.first, 0);
+
+	for (int ii = 0; ii < input.first; ii++) { scanf("%d", &number[ii]); }
+	sort(number.begin(), number.end());
+
+	vector<int> bucket = vector<int>(input.second);
+	reculsive(input.first, input.second, 0, bucket);
+
+	for (const vector<int> print : answer) {
+		for (const int number : print) { printf("%d ", number); }
+		printf("\n");
+	}
+
+	return 0;
+}
